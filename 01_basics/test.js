1541e0ed18176f42a8cafc7080daeebabe720020
Utkarsh Shukla <utkarshshukla24.08@gmail.com>
2025-08-18 14:55:48 +0530

    just learning some basics

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,8 @@
-console.log("Hello World");
-console.log("Bye World");
+// console.log("Hello World");
+// console.log(typeof("Bye World")== String);
+// console.log(Number("3500"))
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
