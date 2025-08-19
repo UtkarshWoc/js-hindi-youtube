6f7befebf3286f8a3f8616a1f44060e1ec6a1de2
Utkarsh Shukla <utkarshshukla24.08@gmail.com>
2025-08-19 15:58:06 +0530

    Learnt about `` character while printing

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,7 +2,17 @@
 // console.log(typeof("Bye World")== String);
 // console.log(Number("3500"))
 
-console.log(undefined == 0)
-console.log(undefined > 0)
-console.log(undefined >= 0)
-console.log(undefined <= 0)
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+// const id=Symbol("Hi");
+// const anotherid=Symbol("Hi");
+// console.log(id ==anotherid );
+
+
+let name="uttu"
+let cnt=50
+console.log(`Hello my name is ${name} and my repo count is ${cnt}`);
+
