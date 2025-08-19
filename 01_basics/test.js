f9cb45bba7227f6b86b1c7f7ae51d78be90d0bc8
Utkarsh Shukla <utkarshshukla24.08@gmail.com>
2025-08-19 16:19:56 +0530

    Using Math.random function

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -12,7 +12,20 @@
 // console.log(id ==anotherid );
 
 
-let name="uttu"
-let cnt=50
-console.log(`Hello my name is ${name} and my repo count is ${cnt}`);
+// let name="uttu"
+// let cnt=50
+// console.log(`Hello my name is ${name} and my repo count is ${cnt}`);
+
+
+// number=1241312
+// console.log(number.toLocaleString('en-US'));
+// console.log(Math.abs(number));
+
+// console.log(Math);
+
+//Using Math.Random Function
+console.log(Math.random()*10 +1);
+min=10; max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
 
