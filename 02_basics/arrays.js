47ae506d51ca1d76cabbacae8d5c5756fde4ca36
Utkarsh Shukla <utkarshshukla24.08@gmail.com>
2025-08-25 09:14:30 +0530

    join, slice, splice fn

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -1,2 +1,32 @@
 let arr=[0,1,2,3,4,5]
-console.log(arr[1]);
+// console.log(arr[1]);
+
+//Array Methods
+
+// arr.push(2)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// arr.unshift(9); //adds 9 in start
+// arr.shift(); //removes 9 from start
+// console.log(arr);
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+
+// const newArr=arr.join();
+// console.log(arr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice & splice
+// const myn1=arr.slice(1,3);
+// console.log(myn1);
+// console.log("B ",arr);
+
+// const myn2=arr.splice(1,3); //splice not only includes range but also makes changes in original array. 
+// console.log(myn2);
+// console.log("B ",arr);
+
+
